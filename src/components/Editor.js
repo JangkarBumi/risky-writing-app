755cd7918d9ecb1412babff06199a3ec75181682
Zeha Irawan <jangkaringbumi@gmail.com>
2020-11-03 16:44:58 +0700

    download feature

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,26 @@
-import React, { Fragment } from 'react'
+import React, {  useState } from 'react'
 
 const Editor = () => {
+
+  const [text,setText] = useState('')
+
+  const handleChange =(e) => {
+    setText(e.target.value)
+  }
+
+  const handleDownload = () => {
+    const element = document.createElement("a");
+    const file = new Blob([text], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "risky-writing-app.txt";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+
   return (
     <div className="editor-container">
-      <input className="editor-input" type="text" name="" id=""/>
+      <button onClick={handleDownload}>Download</button>
+      <input className="editor-input" type="text" name="text" value={text} onChange={handleChange}/>
     </div>
   )
 }
